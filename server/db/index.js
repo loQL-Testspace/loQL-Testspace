79694b6c7ade9682024a4387d9dc2d08ee6441de
lolfill <lolfill0104@gmail.com>
2021-08-16 19:21:38 -0400

    pets resolver and schema

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,16 @@
 import low from 'lowdb'
 import FileSync from 'lowdb/adapters/FileSync.js'
 import createHumanModel from './humans.js';
+import createPetModel from './pet.js';
+
 
 const adapter = new FileSync('server/db/dummy.json');
 export const db = low(adapter);
 
 // other models later
 export const models = {
-  Human: createHumanModel(db)
+  Human: createHumanModel(db),
+  Pet: createPetModel(db)
 }
 
 // export {
