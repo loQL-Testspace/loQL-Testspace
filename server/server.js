bbebc87024fef7944a7444b314a7ab2825a2ae34
Jae Ryu <thejaeryu@gmail.com>
2021-08-17 20:05:22 -0400

    end of tues

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
 import { ApolloServer } from 'apollo-server'
-import typeDefs  from './schema.js'
-import resolvers  from './resolvers.js'
-import {models, db}  from './db/index.js'
+import typeDefs from './schema.js'
+import resolvers from './resolvers.js'
+import { models, db } from './db/index.js'
 // const model = require('./db/dummy');
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context() {
-      const human = db.get('human').value()
-      return {models, db, human}
-    }
+  typeDefs,
+  resolvers,
+  context() {
+    // const human = db.get('human').value()
+    return { models, db }
+  }
 })
 
 server.listen().then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
+  console.log(`🚀 Server ready at ${url}`);
 });
