fa022bb03e178c1749023fa21b8d5a28f90956cb
Jae Ryu <thejaeryu@gmail.com>
2021-08-19 14:55:15 -0400

    before pull

--- client/handlers.js
+++ client/handlers.js
@@ -7,13 +7,7 @@ queryButton.addEventListener("click", (e) => {
             'Accept': 'application/json',
         },
         body: JSON.stringify({
-            query: `
-            query GetHumanQuery($id: String!) {
-                human(input: {id: $id }) {
-                  id
-                }
-              }
-            `,
+            query: `query GetHumanQuery($id: String!) {human(input: {id: $id }) {id}}`,
             variables: {
                 id: "1"
             }
@@ -23,6 +17,50 @@ queryButton.addEventListener("click", (e) => {
   .then(data => console.log('response from query:', data));
     console.log("You clicked the query button.")
 });
+
+// queryButton.addEventListener("click", (e) => {
+//     fetch('/api/graphql', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Accept': 'application/json',
+//         },
+//         body: JSON.stringify({
+//             query: `query GetHumanQuery($id: String!) {human(input: {id: $id }) {id}}`,
+//             variables: {
+//                 id: "1"
+//             }
+//         })
+//     })
+//   .then(r => r.json())
+//   .then(data => console.log('response from query:', data));
+//     console.log("You clicked the query button.")
+// });
+
+// queryButton.addEventListener("click", (e) => {
+//     fetch('/api/graphql', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Accept': 'application/json',
+//         },
+//         body: JSON.stringify({
+//             query: `
+//             query GetHumanQuery($id: String!) {
+//                 human(input: {id: $id }) {
+//                   id
+//                 }
+//               }
+//             `,
+//             variables: {
+//                 id: "1"
+//             }
+//         })
+//     })
+//   .then(r => r.json())
+//   .then(data => console.log('response from query:', data));
+//     console.log("You clicked the query button.")
+// });
 
 const mutationButton = document.getElementById("mutation1");
 mutationButton.addEventListener("click", (e) => {
