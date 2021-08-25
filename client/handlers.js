4805038ab43d20dab0853687fe3ba20cb8413d27
Konstantin <konst@ntinh.am>
2021-08-25 03:06:17 -0400

    added two new queries using pokeapi, confirmed working

--- client/handlers.js
+++ client/handlers.js
@@ -57,3 +57,65 @@ mutationButton.addEventListener('click', (e) => {
   console.log('You clicked the query button.');
   console.log('This is the mutation button.');
 });
+
+const queryPokeSimpButton = document.getElementById('query3');
+queryPokeSimpButton.addEventListener('click', (e) => {
+  fetch('https://beta.pokeapi.co/graphql/v1beta', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      query: `
+            query MyQuery($name: String){
+              pokemon_v2_ability(where: {name: {_eq: $name}}){
+                name
+                id
+                generation_id
+              }
+            }
+          `,
+      variables: {
+        "name": "rock-head"
+      },
+    }),
+  })
+    .then((r) => r.json())
+    .then((data) => console.log('Response from query:', data));
+  console.log('You clicked the query button.');
+});
+
+const queryPokeComplexButton = document.getElementById('query4');
+queryPokeComplexButton.addEventListener('click', (e) => {
+  fetch('https://beta.pokeapi.co/graphql/v1beta', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      query: `
+            query MyQuery {
+              pokemon_v2_location(where: {name: {_eq: "cianwood-city"}}) {
+                pokemon_v2_region {
+                  pokemon_v2_pokedexes {
+                    pokemon_v2_pokemondexnumbers(where: {pokemon_species_id: {_gte: 100}, _and: {pokemon_species_id: {_lte: 110}}}) {
+                      pokemon_v2_pokemonspecy {
+                        capture_rate
+                        gender_rate
+                        name
+                        id
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          `,
+    }),
+  })
+    .then((r) => r.json())
+    .then((data) => console.log('Response from query:', data));
+  console.log('You clicked the query button.');
+});
--- client/index.js
+++ client/index.js
@@ -2,4 +2,4 @@ import { register } from 'loql';
 
 // User passes in configuration object into registration function
 // The function will save these settings to IDB so they can be accessed by the service worker
-register({ useMetrics: false, dummy: false });
+register({ useMetrics: true, cacheExpirationLimit: 1000, doNotCache: ['capture_rate']});
