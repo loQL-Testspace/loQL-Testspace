d98f0ad13109f15d2fd1a245d648a323f2b2a314
Konstantin <konst@ntinh.am>
2021-08-25 17:36:46 -0400

    attempt #2 to add poke-queries

--- client/components/Demo.jsx
+++ client/components/Demo.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { register } from 'loql';
 import { avgDiff, uncahedAvg, cachedAvg, summary } from 'loql/Metrics.js'
-import { query1 } from "../queries";
+import { query1, query3, query4 } from "../queries";
 
 register({ cacheExpirationLimit: 20000, cacheMethod: "cache-network" }); // sw.js
 
@@ -26,6 +26,8 @@ const Demo = () => {
     <div>
       <button className="queries" id="query1" onClick={() => performGQLQuery('/api/graphql', query1)}>Get Human Query </button>
       <button className="queries" id="query2" onClick={() => performGQLQuery('http://localhost:4000/graphql?query=query{human(input:{id:"1"}){name}}')} >Get Human Query </button>
+      <button className="queries" id="query3" onClick={() => performGQLQuery('https://beta.pokeapi.co/graphql/v1beta', query3)}>Simple Pokemon Query </button>
+      <button className="queries" id="query4" onClick={() => performGQLQuery('https://beta.pokeapi.co/graphql/v1beta', query4)}>Nested Pokemon Query </button>
       <button className="mutations" id="mutation1" onClick={() => performGQLQuery('/api/graphql', mutation1)}>Add Human Mutation </button>
       <p>{JSON.stringify(lastQueryData)}</p>
     </div>
--- client/queries/index.js
+++ client/queries/index.js
@@ -18,7 +18,57 @@ export const query1 = {
     }),
   };
 
-  export const mutation1 = {
+export const query3 = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      query: `
+            query MyQuery($name: String){
+              pokemon_v2_ability(where: {name: {_eq: $name}}){
+                name
+                id
+                generation_id
+              }
+            }
+          `,
+      variables: {
+        "name": "rock-head"
+      },
+    }),
+  };
+
+export const query4 = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      query: `
+            query MyQuery {
+              pokemon_v2_location(where: {name: {_eq: "cianwood-city"}}) {
+                pokemon_v2_region {
+                  pokemon_v2_pokedexes {
+                    pokemon_v2_pokemondexnumbers(where: {pokemon_species_id: {_gte: 100}, _and: {pokemon_species_id: {_lte: 110}}}) {
+                      pokemon_v2_pokemonspecy {
+                        capture_rate
+                        gender_rate
+                        name
+                        id
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          `,
+    }),
+  };
+
+export const mutation1 = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
