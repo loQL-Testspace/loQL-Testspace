2479b2adcc26e9d8a7bad7aa98dae29ee07f0733
Andrew Kessinger <andrew.kessinger@gmail.com>
2021-08-30 16:35:21 +0200

    Linted

--- client/components/Graph-shell.jsx
+++ client/components/Graph-shell.jsx
@@ -1,55 +1,48 @@
 import React from 'react';
-import "./Graph-shell.scss";
+import './Graph-shell.scss';
 
+const Graph = ({ metricData }) => {
+  //hardcoded data for now
 
-const Graph = ({metricData}) => {
+  //scenario 1 -- no data fetched yet
+  metricData = { uncachedSpeed: null, cachedSpeed: null };
 
-    //hardcoded data for now
-    
-    //scenario 1 -- no data fetched yet
-    metricData = {uncachedSpeed: null, cachedSpeed: null};
+  //scenario 2 -- uncached data fetched
+  metricData = { uncachedSpeed: 1200, cachedSpeed: null };
 
-    //scenario 2 -- uncached data fetched
-    metricData = {uncachedSpeed: 1200, cachedSpeed: null};
+  //scenario 3 -- both cached and uncached data fetched
+  metricData = { uncachedSpeed: 1200, cachedSpeed: 200 };
 
-    //scenario 3 -- both cached and uncached data fetched
-   metricData = {uncachedSpeed: 1200, cachedSpeed: 200};
-
-
-
-
-    const { uncachedSpeed, cachedSpeed } = metricData;    
-    //calculate time saving, in this example it's 83%
-    const timeSaving = Math.round((uncachedSpeed - cachedSpeed) / uncachedSpeed * 100);
+  const { uncachedSpeed, cachedSpeed } = metricData;
+  //calculate time saving, in this example it's 83%
+  const timeSaving = Math.round(
+    ((uncachedSpeed - cachedSpeed) / uncachedSpeed) * 100
+  );
 
   return (
+    <div className={'chart-container'}>
+      <div className={'charts'}>
+        <div>Fetched from server</div>
 
+        <div className={'bar-container'}>
+          <div className={'bar1'}>&nbsp;</div>
+          <div>{uncachedSpeed / 1000} sec</div>
+        </div>
 
-    <div className={"chart-container"}>
-      
-<div className={"charts"}>
-    
+        <div className={'cache-title'}>Fetched from cache</div>
 
-<div>Fetched from server</div>
-
-<div className={"bar-container"}><div className={"bar1"}>&nbsp;</div><div>{(uncachedSpeed/1000)} sec</div></div>
-
-<div className={"cache-title"}>Fetched from cache</div>
-
-<div className={"bar-container"}><div className={"bar2"}>&nbsp;</div><div>{(cachedSpeed/1000)} sec</div></div>
-
-
-</div>
-
-
-      <div className={"time-savings"}>
-        <div className={"query-stat"}>{timeSaving}%</div>
-        <div className={"query-stat-text"}>time savings</div>
+        <div className={'bar-container'}>
+          <div className={'bar2'}>&nbsp;</div>
+          <div>{cachedSpeed / 1000} sec</div>
+        </div>
       </div>
 
-
+      <div className={'time-savings'}>
+        <div className={'query-stat'}>{timeSaving}%</div>
+        <div className={'query-stat-text'}>time savings</div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Graph;
