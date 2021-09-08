ac754b1c9f763ef1cc3138ea33e86462f08f2dfe
lolfill <lolfill0104@gmail.com>
2021-09-08 15:25:51 -0400

    added media queries for main page adjusted navbar and footer spacing

--- client/containers/MainContainer.jsx
+++ client/containers/MainContainer.jsx
@@ -10,7 +10,7 @@ const SplashPage = () => {
       <Container>
         <article className="main">
           <div className="splash">
-            <div>
+            <div id="main-left">
               <div className="bigTitle">Fast, performant GraphQL caching</div>
               <div className="bigDescription">
                 loQL is a lightweight, open source npm package that caches API
@@ -18,7 +18,7 @@ const SplashPage = () => {
                 enabling offline use.
               </div>
             </div>
-            <div>
+            <div id="main-right">
               <img src={splashImg} className="splash-img"></img>
             </div>
           </div>
