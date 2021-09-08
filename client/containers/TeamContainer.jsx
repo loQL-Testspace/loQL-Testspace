0c9466247c12af2a2817d0a3cf1b1eac3e00fd08
lolfill <lolfill0104@gmail.com>
2021-09-07 20:34:23 -0400

    test page

---
+++ client/containers/TeamContainer.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import TeamMembers from '../components/TeamMembers'
+import './TeamContainer.scss'
+
+const Team = () => {
+
+  return (
+    <div id="team-content">
+      <div id="team-summary">
+        <div>
+          <h1>
+            Meet the team behind LOQL
+          </h1>
+          <p className="subtext">
+            LOQL is developed and maintained by a group of full stack engineers 
+            that are passionate about learning new technologies and creating
+            useful developer tools.
+          </p>
+        </div>
+        <img /> {/*Some sort of logo image*/}
+        
+      </div>
+      <div id="team-container">
+        <TeamMembers id="team-cards" />
+      </div>
+    </div>
+  )
+}
+ 
+export default Team;
