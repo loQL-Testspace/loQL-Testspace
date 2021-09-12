17d1f969810c6e756d2fc1d875471091e76a71ab
Harrison Cramer <harrisoncramer@gmail.com>
2021-09-12 10:28:03 -0700

    Fixed import for video and images.

--- webpack.dev.js
+++ webpack.dev.js
@@ -15,17 +15,6 @@ export default merge(common, {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(png|jpg|gif|jpeg|ico|mp4)$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192,
-            },
-          },
-        ],
-      },
     ],
   },
   devServer: {
